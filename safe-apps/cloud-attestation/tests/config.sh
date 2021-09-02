34c41f47edc1a887b7ee91cd09bb3e506c0255f0
Dr. Jeffrey Chase <chase@cs.duke.edu>
2021-09-02 14:30:18 -0400

    some fixes to config slang and testing

--- safe-apps/cloud-attestation/tests/config.sh
+++ safe-apps/cloud-attestation/tests/config.sh
@@ -2,13 +2,13 @@
 export SAFE_ADDR=http://localhost:7777
 . ../cfunctions
 
-postProhibited cset1 image_c1 '[k5]'
-postQualifier cset1 image_c1 '[[k1,v1]]'
-postRequired cset1 image_c1 '[k1,k2]'
-postProhibited cset1 image_c2 '[k5]'
-postQualifier cset1 image_c2 '[[k4,v4]]'
-postRequired cset1 image_c2 '[k3]'
+postProhibited strong-1 cset1 image_c1 '[k5]'
+postQualifier strong-1 cset1 image_c1 '[[k1,v1]]'
+postRequired strong-1 cset1 image_c1 '[k1,k2]'
+postProhibited strong-1 cset1 image_c2 '[k5]'
+postQualifier strong-1 cset1 image_c2 '[[k4,v4]]'
+postRequired strong-1 cset1 image_c2 '[k3]'
 
-postInstanceConfig kmaster pod1 subinstances '[ctn1,ctn2]'
-postSubinstanceConfig kmaster ctn1 pod1 image_c1 '[[k1,v1],[k2,v2]]'
-postSubinstanceConfig kmaster ctn2 pod1 image_c2 '[[k3,v3],[k4,v4]]'
+postInstanceConfig strong-2 pod1 subinstances '[ctn1,ctn2]'
+postSubinstanceConfig strong-2 ctn1 pod1 image_c1 '[[k1,v1],[k2,v2]]'
+postSubinstanceConfig strong-2 ctn2 pod1 image_c2 '[[k3,v3],[k4,v4]]'
